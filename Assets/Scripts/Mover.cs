973b2a18ddb20eb4a25f42dea9e603f4b1ce3893
blanck_somnia <sedusovasnezana@gmail.com>
2024-08-04 20:44:03 +0500

    MoverDone

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,34 +1,47 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Mover : MonoBehaviour
 {
-	[SerializeField]
-	private float _moveTime = 1f;
-	[SerializeField]
-	private float _delayTime = 2f;
-	[SerializeField]
-	private Vector3[] _positions;
-
+    [SerializeField]
+    private Vector3 _start;
+    [SerializeField]
+    private Vector3 _end;
+    [SerializeField]
+    private float _speed = 10f;
+    [SerializeField]
+    private float _delay = 1f;
 	private IEnumerator Start()
     {
-		if(_positions.Length < 2) yield break;
-		int prev = 0, curr = 1;
-		var time = 0f;
-		var transform = this.transform;
-		while(true)
+        var lenghtDistance = Vector3.Distance(_start, _end);
+		var startTime = Time.time;
+        var Start = _start;
+        var End = _end;
+        while (true)
 		{
-			transform.position = Vector3.Lerp(_positions[prev], _positions[curr], time / _moveTime);
-			time += Time.deltaTime;
-			if(time >= _moveTime)
-			{
-				time = 0f;
-				prev = curr;
-				curr = (curr + 1) % _positions.Length;
-				yield return new WaitForSeconds(_delayTime);
-			}
+            var distance = (Time.time - startTime) * _speed;
+            var moveTime = distance / lenghtDistance;
+            transform.position = Vector3.Lerp(Start, End, moveTime);
 
-			yield return null;
-		}
+            if(transform.position == _end)
+            {
+                yield return new WaitForSeconds(_delay);
+                startTime = Time.time;
+                Start = _end;
+                End = _start;
+                
+            }
+            if(transform.position == _start)
+            {
+                yield return new WaitForSeconds(_delay);
+                startTime = Time.time;
+                Start = _start;
+                End = _end;
+                
+            }
+            yield return null;
+        }
 	}
 }
